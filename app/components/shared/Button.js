7b14ccbb14cb28c3a1fbf2499114858125d808a2
bill-gao <bill.gao@rea-group.com>
2016-06-07 19:39:30 +1000

    Make button a stateless component

--- app/components/shared/Button.js
+++ app/components/shared/Button.js
@@ -1,34 +1,26 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 import {
     Text,
     TouchableHighlight
 } from 'react-native';
 
-class Button extends Component {
-  constructor(props) {
-    super(props);
+const Button = ({theme, disabled}) => {
+  const {styles} = theme;
+  const btnStyles = [styles.button];
+  if (disabled) {
+    btnStyles.push(styles.buttonDisabled);
   }
 
-  render() {
-    const {theme, disabled} = this.props;
-    const {styles} = theme;
-
-    const btnStyles = [styles.button];
-    if (disabled) {
-      btnStyles.push(styles.buttonDisabled);
-    }
-
-    return (
-      <TouchableHighlight
-          onPress={this.props.onPress}
-          style={btnStyles}
-          underlayColor="#a30000"
-          activeOpacity={1}>
-        <Text style={styles.buttonText}>{this.props.children}</Text>
-      </TouchableHighlight>
-    );
-  }
-}
+  return (
+    <TouchableHighlight
+        onPress={this.props.onPress}
+        style={btnStyles}
+        underlayColor="#a30000"
+        activeOpacity={1}>
+      <Text style={styles.buttonText}>{this.props.children}</Text>
+    </TouchableHighlight>
+  );
+};
 
 Button.propTypes = {
   styles: PropTypes.object,
